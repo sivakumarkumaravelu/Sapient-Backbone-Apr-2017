280af577519b9990092ea655f0e97df1235807a6
Magesh <tkmagesh77@gmail.com>
2017-04-21 16:39:46 +0530

    Day-01 Complete

---
+++ Day-01/02-Closures/isPrime.js
@@ -0,0 +1,47 @@
+var isPrime = (function(){
+	var cache = {};
+
+	function isPrime(n){
+		if (typeof cache[n] !== 'undefined')
+			return cache[n];
+		console.log('processing ', n);
+		cache[n] = true;
+		for(var index = 2; index <= (n/2); index++){
+			if (n % index === 0){
+				cache[n] = false;
+				return cache[n];
+			}
+		}
+		return cache[n];
+	}
+	return isPrime;
+})();
+
+
+var isPrime = (function(){
+	var cache = {};
+	
+	function checkPrime(n){
+		console.log('processing ', n);
+		if (n <= 3) return true;
+		for(var index = 2; index <= (n/2); index++){
+			if (n % index === 0)
+				return false;
+		}
+		return  true;
+	}
+	return function(n){
+		if (typeof cache[n] === 'undefined')
+			cache[n] = checkPrime(n);
+		return cache[n];
+	}
+})();
+
+
+
+
+
+
+
+
+
