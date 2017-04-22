27f4ed4ebc86aeee0966feec7b3ee205c644dfd3
Magesh <tkmagesh77@gmail.com>
2017-04-22 09:57:08 +0530

    memoize with varying number of arguments

--- Day-01/02-Closures/isPrime.js
+++ Day-01/02-Closures/isPrime.js
@@ -74,7 +74,15 @@ var isPrime = memoize(function (n){
 	return  true;
 });
 
-
+function memoize(algoFn){
+	var cache = {};
+	return function(){
+		var key = JSON.stringify(arguments);
+		if (typeof cache[key] === 'undefined')
+			cache[key] = algoFn.apply(this, arguments);
+		return cache[key];
+	}
+}
 
 
 
